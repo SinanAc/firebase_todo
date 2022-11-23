4ff1f1a3aecc01ee433801aa598a332ab31518f8
Sinan <sinanac124@gmail.com>
2022-11-23 23:22:50 +0530

    styles added

---
+++ lib/utils/styles.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppStyle {
+  // ===>>> app colors
+  static const Color bgColor = Color(0xFFe2e2ef);
+  static const Color mainColor = Color(0xFF000633);
+  static const Color accentColor = Color(0xFF0065FF);
+  static List<Color> cardColor = [
+    Colors.white,
+    Colors.red.shade100,
+    Colors.pink.shade100,
+    Colors.orange.shade100,
+    Colors.yellow.shade100,
+    Colors.green.shade100, 
+    Colors.blue.shade100,
+    Colors.blueGrey.shade100,
+  ];
+
+  // ===>>> app text styles
+  static TextStyle mainTitleStyle = GoogleFonts.roboto(
+    fontSize: 18.0,
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle contentStyle = GoogleFonts.nunito(
+    fontSize: 16.0,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle dateTitleStyle = GoogleFonts.roboto(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w500,
+  );
+}
