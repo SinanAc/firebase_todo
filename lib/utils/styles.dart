54c73263ff59f47758cb8f25e01e9b10238505fe
Sinan <sinanac124@gmail.com>
2022-11-24 11:11:16 +0530

    firestore connected

--- lib/utils/styles.dart
+++ lib/utils/styles.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppStyle {
-  // ===>>> app colors
+  // ===>>> COLORS
   static const Color bgColor = Color(0xFFe2e2ef);
   static const Color mainColor = Color(0xFF000633);
   static const Color accentColor = Color(0xFF0065FF);
@@ -12,21 +12,39 @@ class AppStyle {
     Colors.pink.shade100,
     Colors.orange.shade100,
     Colors.yellow.shade100,
-    Colors.green.shade100, 
+    Colors.green.shade100,
     Colors.blue.shade100,
     Colors.blueGrey.shade100,
   ];
 
-  // ===>>> app text styles
+  // ===>>> HEIGHT SPACES
+  static const appHeight_10 = SizedBox(height: 10);
+  static const appHeight_15 = SizedBox(height: 15);
+  static const appHeight_20 = SizedBox(height: 20);
+
+  // ===>>> WIDTH SPACES
+  static const appWidth_10 = SizedBox(width: 10);
+  static const appWidth_15 = SizedBox(width: 15);
+  static const appWidth_20 = SizedBox(width: 20);
+
+  // ===>>> TEXT STYLES
+  static TextStyle appTitleStyle = GoogleFonts.roboto(
+    color: Colors.white,
+    fontSize: 22.0,
+    fontWeight: FontWeight.bold,
+  );
   static TextStyle mainTitleStyle = GoogleFonts.roboto(
+    color: Colors.white,
     fontSize: 18.0,
     fontWeight: FontWeight.bold,
   );
   static TextStyle contentStyle = GoogleFonts.nunito(
+    color: Colors.white,
     fontSize: 16.0,
     fontWeight: FontWeight.normal,
   );
   static TextStyle dateTitleStyle = GoogleFonts.roboto(
+    color: Colors.white,
     fontSize: 14.0,
     fontWeight: FontWeight.w500,
   );
--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_todo/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +6,30 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: AppStyle.mainColor,
+      appBar: AppBar(
+        elevation: 0,
+        title: Text(
+          'Fire notes',
+          style: AppStyle.mainTitleStyle,
+        ),
+        centerTitle: true,
+        backgroundColor: AppStyle.mainColor,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Recent notes', style: AppStyle.appTitleStyle),
+            AppStyle.appHeight_20,
+            // StreamBuilder<QuerySnapshot>(
+            //   builder: (context, snapshot) {},
+            // )
+          ],
+        ),
+      ),
+    );
   }
-} 
+}
